5b946d048b505c7e84ed0f8873044efd72b280d4
Ken Kelly <ken@kenhkelly.us>
2015-09-18 16:17:43 -0400

    Add more completed lessons

---
+++ goroutines.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+
+func main() {
+
+	go f("goroutine first")
+
+	f("direct")
+
+	go f("goroutine")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println("done")
+
+}
